8cbbd53d5a0d668c7af6e2bd1ccfce2a42d09e28
Ashar Fuadi <fushar@gmail.com>
2015-02-05 15:18:36 +0700

    Implement submission

---
+++ app/org/iatoki/judgels/uriel/ContestSubmission.java
@@ -0,0 +1,87 @@
+package org.iatoki.judgels.uriel;
+
+import org.iatoki.judgels.commons.Submission;
+import org.iatoki.judgels.gabriel.Verdict;
+
+public final class ContestSubmission implements Submission {
+
+    private final long id;
+    private final String jid;
+    private final String problemJid;
+    private final String contestJid;
+    private final String authorJid;
+    private final String gradingLanguage;
+    private final String gradingEngine;
+    private final long timestamp;
+    private final Verdict verdict;
+    private final int score;
+    private final String details;
+
+    public ContestSubmission(long id, String jid, String problemJid, String contestJid, String authorJid, String gradingLanguage, String gradingEngine, long timestamp, Verdict verdict, int score, String details) {
+        this.id = id;
+        this.jid = jid;
+        this.problemJid = problemJid;
+        this.contestJid = contestJid;
+        this.authorJid = authorJid;
+        this.gradingLanguage = gradingLanguage;
+        this.gradingEngine = gradingEngine;
+        this.timestamp = timestamp;
+        this.verdict = verdict;
+        this.score = score;
+        this.details = details;
+    }
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public String getJid() {
+        return jid;
+    }
+
+    @Override
+    public String getProblemJid() {
+        return problemJid;
+    }
+
+    @Override
+    public String getContestJid() {
+        return contestJid;
+    }
+
+    @Override
+    public String getAuthorJid() {
+        return authorJid;
+    }
+
+    @Override
+    public String getGradingLanguage() {
+        return gradingLanguage;
+    }
+
+    public String getGradingEngine() {
+        return gradingEngine;
+    }
+
+    @Override
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public Verdict getVerdict() {
+        return verdict;
+    }
+
+    @Override
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public String getDetails() {
+        return details;
+    }
+}
