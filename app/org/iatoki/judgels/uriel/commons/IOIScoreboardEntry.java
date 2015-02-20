692f5e8e7a4ae8b1abee28f27ca4fcf4f815bfd1
Ashar Fuadi <fushar@gmail.com>
2015-02-20 15:44:46 +0700

    Implement IOI scoreboard

--- app/org/iatoki/judgels/uriel/commons/IOIScoreboardEntry.java
+++ app/org/iatoki/judgels/uriel/commons/IOIScoreboardEntry.java
@@ -2,26 +2,18 @@ package org.iatoki.judgels.uriel.commons;
 
 import java.util.Map;
 
-public final class IOIScoreboardEntry implements ScoreboardEntry {
-    private final String contestantJid;
-    private final Map<String, Integer> scoresByProblemJid;
-    private final int totalScores;
+public final class IOIScoreboardEntry implements ScoreboardEntry, Comparable<IOIScoreboardEntry> {
+    public int rank;
+    public String contestantJid;
+    public Map<String, Integer> scoresByProblemJid;
+    public int totalScores;
 
-    public IOIScoreboardEntry(String contestantJid, Map<String, Integer> scoresByProblemJid, int totalScores) {
-        this.contestantJid = contestantJid;
-        this.scoresByProblemJid = scoresByProblemJid;
-        this.totalScores = totalScores;
-    }
+    @Override
+    public int compareTo(IOIScoreboardEntry o) {
+        if (totalScores != o.totalScores) {
+            return o.totalScores - totalScores;
+        }
 
-    public String getContestantJid() {
-        return contestantJid;
-    }
-
-    public Map<String, Integer> getScoresByProblemJid() {
-        return scoresByProblemJid;
-    }
-
-    public int getTotalScores() {
-        return totalScores;
+        return contestantJid.compareTo(o.contestantJid);
     }
 }
