c43a8dc5966ee3438e6f4f65e33dede2e05123b4
Ashar Fuadi <fushar@gmail.com>
2015-03-17 15:45:55 +0700

    Implement programming language restriction
    Part of ia-toki/judgels#27

--- app/org/iatoki/judgels/uriel/ContestStyleConfigIOI.java
+++ app/org/iatoki/judgels/uriel/ContestStyleConfigIOI.java
@@ -1,9 +1,23 @@
 package org.iatoki.judgels.uriel;
 
+import org.iatoki.judgels.sandalphon.commons.programming.LanguageRestriction;
+
 public class ContestStyleConfigIOI implements ContestStyleConfig {
+    private final LanguageRestriction languageRestriction;
 
     public static ContestStyleConfigIOI defaultConfig(Contest contest) {
-        return new ContestStyleConfigIOI();
+        return new ContestStyleConfigIOI(LanguageRestriction.defaultRestriction());
     }
 
+    public ContestStyleConfigIOI(LanguageRestriction languageRestriction) {
+        this.languageRestriction = languageRestriction;
+    }
+
+    public LanguageRestriction getLanguageRestriction() {
+        if (languageRestriction == null) {
+            return LanguageRestriction.defaultRestriction();
+        } else {
+            return languageRestriction;
+        }
+    }
 }
