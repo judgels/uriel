4b011a236d3448f27152671b72fd25ae0254a2ab
Ashar Fuadi <fushar@gmail.com>
2015-07-13 13:49:01 +0700

    Revert private variable state -> config
    Because it is used already in JSONs in database

--- app/org/iatoki/judgels/uriel/IOIScoreboard.java
+++ app/org/iatoki/judgels/uriel/IOIScoreboard.java
@@ -1,17 +1,19 @@
 package org.iatoki.judgels.uriel;
 
 public class IOIScoreboard implements Scoreboard {
-    private final ScoreboardState state;
+    // If you want to fix this variable's name, you have to fix the scoreboard JSON data in database!
+    private final ScoreboardState config;
+
     private final IOIScoreboardContent content;
 
     public IOIScoreboard(ScoreboardState state, IOIScoreboardContent content) {
-        this.state = state;
+        this.config = state;
         this.content = content;
     }
 
     @Override
     public ScoreboardState getState() {
-        return state;
+        return config;
     }
 
     @Override
